860832ddb0ea8c3417759e427665fddb35a56a20
Lucas Rafael <3lucasrs@gmail.com>
2024-04-30 15:04:19 -0300

    Added SQL script file

---
+++ backend/SQL.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS `taskpane`;
+
+USE `taskpane`;
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `taskName` VARCHAR(255) NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `priority` INT NOT NULL CHECK (`priority` BETWEEN 1 AND 4),
+  `status` INT NOT NULL CHECK (`status` BETWEEN 1 AND 4),
+  PRIMARY KEY (`id`)
+);
